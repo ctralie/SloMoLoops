77e0ec8e84557fe63d966ad5281e2ff3c6ce47bf
Chris Tralie <chris.tralie@gmail.com>
2017-11-09 14:15:30 -0500

    Fixing consensus reordering, and incorporating TDA to find appropriate threshold

--- 1DExamples.py
+++ 1DExamples.py
@@ -4,6 +4,7 @@ import scipy.io as sio
 import scipy.interpolate as interp
 from VideoReordering import *
 from sklearn.decomposition import PCA
+from ripser import ripser
 
 def drawLineColored(idx, x, C):
     plt.hold(True)
@@ -41,56 +42,72 @@ def getReorderedConsensus(X, N, theta):
         tu = -tu
     tu = tu - np.min(tu)
     NPeriods = int(np.round(np.max(tu)/(2*np.pi)*N/M))
+    T = N/NPeriods #Period
     print "NPeriods = ", NPeriods
-    tu = np.mod(tu, 2*np.pi)
+    print "N/NPeriods = ", float(N)/NPeriods
+    tu = N*np.mod(tu, 2*np.pi)/(2*np.pi)
     idx = np.argsort(tu)
     X2 = X[idx, :]
     t1 = tu[idx]
-
-    t2 = np.linspace(0, 2*np.pi, M)
-    f = interp.interp2d(np.arange(X2.shape[1]), t1, X2, kind='linear')
-    Y = f(np.arange(X2.shape[1]), t2)
-    Z = np.nan*np.ones((d, N))
-    for i in range(d):
-        idx = np.arange(M) + i*NPeriods
-        idx = np.mod(idx, N)
-        Z[i, idx] = Y[:, i]
+    Z = np.nan*np.ones((M, N))
+    for i in range(M):
+        ts = t1[i] + NPeriods*np.arange(d)
+        imin = int(np.ceil(np.min(ts)))
+        imax = int(np.floor(np.max(ts)))
+        t2 = np.arange(imin, imax+1)
+        x = interp.spline(ts, X2[i, :], t2)
+        Z[i, np.mod(t2, N)] = x
     plt.imshow(Z, aspect = 'auto', interpolation = 'none', cmap = 'afmhot')
+    plt.xlabel("Time")
+    plt.ylabel("Spline Interpolated Windows")
     plt.show()
     z = np.nanmedian(Z, 0)
     return z
 
 if __name__ == '__main__':
     np.random.seed(100)
-    Weighted = True
+    Weighted = False #Whether to use the weighted graph laplacian
     NPeriods = 20
     SamplesPerPeriod = 9
     N = NPeriods*SamplesPerPeriod
     t = np.linspace(0, 2*np.pi*NPeriods, N)#N+5)[0:N]
     t2 = np.linspace(0, 2*np.pi, N)
     x = np.cos(t) + np.cos(3*t) + np.cos(4*t)
-    x = x + 0.3*np.random.randn(len(x))
+    x = x + 0.5*np.random.randn(len(x))
     x2 = np.cos(t2) + np.cos(3*t2) + np.cos(4*t2)
 
-    dim = SamplesPerPeriod
+    dim = 20
     Tau = 1
     dT = 1
     (X, xidx) = getSlidingWindow(x, dim, Tau, dT)
-    X = X - np.mean(X, 1)[:, None]
-    X = X/np.sqrt(np.sum(X**2, 1))[:, None]
-    sio.savemat("X.mat", {"X":X})
+    #X = X - np.mean(X, 1)[:, None]
+    #X = X/np.sqrt(np.sum(X**2, 1))[:, None]
     extent = Tau*dim
-
-    #Compute reordering based on Laplacian
-    kappa = 0.1
+    
+    #Use rips filtration to guide Laplacian
     D = getSSM(X)
+    Is = ripser.doRipsFiltrationDM(D, 1, coeff=41)
+    I = Is[1]
+    thresh = np.argmax(I[:, 1] - I[:, 0])
+    thresh = I[thresh, 0]
     if Weighted:
-        A = getW(D, kappa)
+        A = np.exp(-D**2/(2*thresh**2))
     else:
-        A = CSMToBinaryMutual(D, kappa)
-    (w, v) = getLaplacianEigsDense(A, 4)
+        A = np.zeros(D.shape)
+        A[D <= thresh] = 1
+        np.fill_diagonal(A, 0)
+    (w, v) = getLaplacianEigsDense(A, 3)
+    
+    #Compute reordering based on Laplacian
+
+    
     v = v[:, [1, 2]]
     theta = np.arctan2(v[:, 1], v[:, 0])
+    
+    ##Ground truth
+    #theta = np.mod(t[0:len(theta)], 2*np.pi)
+    ##
+    
     ridx = np.argsort(theta)
     xresort = x[ridx]
 
@@ -109,34 +126,23 @@ if __name__ == '__main__':
 
 
     fig = plt.figure(figsize=(18, 12))
+    ylims = [-2, 3.5]
     plt.subplot(241)
     drawLineColored(t, x, C[xidx, :])
     ax = plt.gca()
     plotbgcolor = (0.15, 0.15, 0.15)
     ax.set_axis_bgcolor(plotbgcolor)
-    plt.ylim([-3, 3])
+    plt.ylim(ylims)
     plt.title("Original Signal")
     plt.xlabel("t")
 
     #ax2 = fig.add_subplot(132, projection = '3d')
     plt.subplot(242)
-    plt.title("Sliding Window Adjacency Matrix\nWin = %i, $\kappa = %g$"%(dim, kappa))
+    plt.title("Sliding Window Adjacency Matrix\nWin = %i, thresh = %g"%(dim, thresh))
     plt.imshow(1-A, cmap='gray')
 
 
     plt.subplot(243)
-    #drawLineColored(np.arange(len(xresort)), xresort, C[ridx])
-    t = np.unwrap(theta)
-    plt.scatter(t, xresort, c = C[ridx, :], edgecolor = 'none')
-    ax = plt.gca()
-    plotbgcolor = (0.15, 0.15, 0.15)
-    ax.set_axis_bgcolor(plotbgcolor)
-    plt.ylim([-3, 3])
-    plt.xlim([np.min(t), np.max(t)])
-    plt.xlabel("t")
-    plt.title("Reordered Signal")
-
-    plt.subplot(245)
     plt.scatter(v[:, 0], v[:, 1], 20, c=C, edgecolor = 'none')
     plt.xlabel("Laplacian Eigenvector 1")
     plt.ylabel("Laplacian Eigenvector 2")
@@ -146,14 +152,30 @@ if __name__ == '__main__':
     ax.set_xticks([])
     ax.set_yticks([])
 
-    plt.subplot(246)
+    plt.subplot(244)
     plt.plot(theta)
+    plt.title("Circular Coordinates")
+    
+    plt.subplot(245)
+    ripser.plotDGM(I)
+    plt.title("H1")
+
+    plt.subplot(246)
+    plt.plot(x2)
+    plt.ylim(ylims)
+    plt.title("Ground Truth")
 
     plt.subplot(247)
-    plt.plot(x2)
-    plt.ylim([-3, 3])
+    #drawLineColored(np.arange(len(xresort)), xresort, C[ridx])
+    t = np.unwrap(theta)
+    #plt.scatter(t, xresort, c = C[ridx, :], edgecolor = 'none')
+    plt.plot(xresort)
+    ax = plt.gca()
+    plt.ylim(ylims)
+    plt.title("Reordered Signal")
 
-
-    plt.subplot(244)
+    plt.subplot(248)
     plt.plot(y)
+    plt.ylim(ylims)
+    plt.title("Spline Consensus reordering")
     plt.show()
