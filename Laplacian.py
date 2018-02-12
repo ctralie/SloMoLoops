9730c4a7e81a1d585d94ff8f734e04f6fb3aa579
Chris Tralie <chris.tralie@gmail.com>
2018-02-12 10:10:10 -0500

    Submitted version of paper

--- Laplacian.py
+++ Laplacian.py
@@ -50,22 +50,24 @@ def getSlopes(thetas, sWin = 10):
     slopes[-(sWin+1):] = slopes[-(sWin+1)]
     return slopes
 
-def getSlowestEigenvectorIdx(v, tol = 0.95):
+def getSlowestEigenvectorIdx(v, tol = 0.8):
     """
     Find the adjacent pair of eigenvectors with similar eigenvalues that have
     the smallest number of zero crossings
     :param v: NEigs x k array of eigenvectors arrange along columns
     :param tol: Reject pairs with zero crossings ratio less than tol
     """
-    #Compute zero crossings and put the low frequencies eigenvectors first
-    s = v > 0
-    zcs = np.sum(s[1::, :] - s[0:-1, :], 0)
+    #Compute circular zero crossings and put the low frequencies eigenvectors first
+    s = 1.0*(v > 0)
+    zcs = np.sum(np.abs(np.sign(s[1::, :]-s[0:-1, :])), 0)
     smallest = np.inf
     i1 = 1
     for i in range(1, len(zcs)-1):
-        ratio = min(float(zcs[i])/zcs[i+1], float(zcs[i+1])/zcs[i])
-        if ratio < tol:
-            continue
+        if tol > -1:
+            ratio = min(float(zcs[i])/zcs[i+1], float(zcs[i+1])/zcs[i])
+            print("%i and %i, %g"%(i, i+1, ratio))
+            if ratio < tol:
+                continue
         bothSum = zcs[i] + zcs[i+1]
         if bothSum < smallest:
             smallest = bothSum
